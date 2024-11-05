70e3a606fb70cf9e4e60c0acfa38b2eac42ceff5
Peedee01 <piethein@schouten.nl>
2024-11-05 17:06:26 +0100

    footer

---
+++ src/components/githubImage.jsx
@@ -0,0 +1,15 @@
+import Github from "/src/assets/github.png"
+
+function GithubImage() {
+  return (
+    <a href="#">
+      <img
+        src= {Github}
+        alt="Github-Image"
+        width="100"
+      />
+    </a>
+  )
+}
+
+export default GithubImage
