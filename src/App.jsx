4a769b92e3002bc30e70f24d7c1e19a50b2a8d86
Peedee01 <piethein@schouten.nl>
2024-11-05 17:32:54 +0100

    first merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
-import './App.css'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
+      <Navbar />
+      <section id="recipes"></section>
+      <section id="about"></section>
+      <section id="contact"></section>
 
+
+
+    <Footer />
     </>
   );
 }
-
 export default App;
