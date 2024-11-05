576f5c0f4e794cf3dbc8d80704778c4d4d0fb989
Diego Cisneros <dcisnerosg28@gmail.com>
2024-11-05 17:50:30 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <section id="recipe-book"></section>
-      <section id="recipes"></section>
-      <section id="about"></section>
       <section id="contact"></section>
       <SideBar />
       <Footer />
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,12 +18,6 @@ const Navbar = () => {
             </div>
             <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
                 <li>
-                    <a href="/recipes">Recipes</a> 
-                </li>
-                <li>
-                    <a href="/about">About</a> 
-                </li>
-                <li>
                     <a href="/contact">Contact</a> 
                 </li>
             </ul>
