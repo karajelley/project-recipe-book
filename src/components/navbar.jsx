8c56d7a70bf3b707323262f8bdc04a8c46ebd091
Kara Jelley <karajelley@gmail.com>
2024-11-06 11:04:17 -0500

    added logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "./Navbar.css"; 
 import InstagramLogo from "../assets/Instagram_logo_2016.svg"; 
+import appLogo from "../assets/app-logo.png"
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -11,8 +12,11 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <div className="burger-menu" onClick={toggleMenu}>
+{/*             <div className="burger-menu" onClick={toggleMenu}>
                 <span>&#9776;</span> 
+            </div> */}
+            <div className='app-logo'>
+                <img src={appLogo} alt="App Logo" width="250" /> 
             </div>
             <div className="navbar-image">
                 <img src={InstagramLogo} alt="Instagram Logo" width="40" /> 
