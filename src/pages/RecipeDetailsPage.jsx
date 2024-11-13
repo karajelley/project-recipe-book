f50b2dc8a6a7ab8d29451c81550e2a38b887d41a
Diego Cisneros <dcisnerosg28@gmail.com>
2024-11-13 23:22:03 +0100

    added serving/calories counter to the RecipeDetailPage

--- src/pages/RecipeDetailsPage.jsx
+++ src/pages/RecipeDetailsPage.jsx
@@ -1,29 +1,50 @@
 import "../pages/RecipeDetails.css";
-
 import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function RecipeDetailsPage({ data }) {
   const { id } = useParams();
   const recipe = data.find((recipe) => recipe.id === id);
 
+  const [servings, setServings] = useState(recipe.servings);
+  const [calories, setCalories] = useState(recipe.calories);
+
+  const handleIncreaseServings = () => {
+    const newServings = servings + 1;
+    setServings(newServings);
+    setCalories((recipe.calories / recipe.servings) * newServings);
+  };
+
+  const handleDecreaseServings = () => {
+    if (servings > 1) { 
+      const newServings = servings - 1;
+      setServings(newServings);
+      setCalories((recipe.calories / recipe.servings) * newServings);
+    }
+  };
+
   return (
     <div className="details-container">
-        <article>
-          <h3>{recipe.name} - Recipe: </h3>
-          <p>Description: {recipe.description}</p>
-          <p>Ingredients: {recipe.ingredients}</p>
-          <p>Prep: {recipe.preparation}</p>
-          <p>Amount of Calories: {recipe.calories}</p>
-          <p>Servings: {recipe.servings}</p>
-          <img src={recipe.image} alt={recipe.name} />  
-        <NavLink to={`/edit-recipe/${id}`}> 
-          <button> Edit</button>
-        </NavLink>
-        </article>
+      <article>
+        <h3>{recipe.name} - Recipe:</h3>
+        <p>Description: {recipe.description}</p>
+        <p>Ingredients: {recipe.ingredients.join(", ")}</p>
+        <p>Prep: {recipe.preparation}</p>
+        <p>Servings: {servings}</p>
 
-      
-      
+        <div className="servings-controls">
+          <button onClick={handleDecreaseServings}>-</button>
+          <button onClick={handleIncreaseServings}>+</button>
+        </div>
+
+        <p>Calories: {calories}</p>
+        <img src={recipe.image} alt={recipe.name} />
+        
+        <NavLink to={`/edit-recipe/${id}`}>
+          <button>Edit</button>
+        </NavLink>
+      </article>
     </div>
   );
 }
